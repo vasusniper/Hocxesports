1498eb44bb175a084ff363a7994d8a5a54484ea9
Vasu Sniper <vasusniper@gmail.com>
2025-06-23 06:09:43 +0530

    Fix: corrected import casing for ShowTeamProfile

--- Frontend/src/Components/Tournament/tournamentPage.jsx
+++ Frontend/src/Components/Tournament/tournamentPage.jsx
@@ -2,15 +2,12 @@ import React from "react";
 import TournamentList from "./tournamentList";
 import "./tournamentPage.css"
 import ShowTeamProfile from "../PlayerList/showTeamProfile";
-
 const tournamentPage = () => {
   return (
     <div className="tournamentPage-container">
-    
         <ShowTeamProfile/>
         <TournamentList />
     </div>
   );
 };
-
 export default tournamentPage;
