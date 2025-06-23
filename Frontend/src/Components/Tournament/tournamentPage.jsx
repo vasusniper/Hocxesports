f783dc9167dcde8036c85f9664b4e022a6e8f2a1
Vasu Sniper <vasusniper@gmail.com>
2025-06-23 06:24:00 +0530

    Fix: Corrected casing in ShowTeamProfile import

--- Frontend/src/Components/Tournament/tournamentPage.jsx
+++ Frontend/src/Components/Tournament/tournamentPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import TournamentList from "./tournamentList";
-import ShowTeamProfile from "../PlayerList/showTeamProfile";
+import ShowTeamProfile from "../PlayerList/showTeamProfile"
 import "./tournamentPage.css"
 const tournamentPage = () => {
   return (
